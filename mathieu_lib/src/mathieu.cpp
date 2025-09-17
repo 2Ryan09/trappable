9a625cbb497dd2b4f01dc0af2aea786a304e6643
Ryan <ryan@waldhe.im>
2025-09-17 11:46:09 -0700

    fix build warnings

--- mathieu_lib/src/mathieu.cpp
+++ mathieu_lib/src/mathieu.cpp
@@ -16,6 +16,10 @@ constexpr double AVOGADRO_NUMBER = 6.02214076E23;  // Avogadro's number in mol^-
 constexpr double MIN_Q = 0.25;                     // Minimum stable q value for quadrupole
 constexpr double MAX_Q = 0.908;                    // Maximum stable q value for quadrupole
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 /**
  * @namespace mathieu_lib
  * @brief Namespace for Mathieu-related mathematical functions.
